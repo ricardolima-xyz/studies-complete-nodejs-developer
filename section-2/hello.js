54001126a4daa6857b910a0e00ba6e89417fc4c5
Ricardo Lima <ricardo@lionshome.de>
2024-01-16 12:58:46 +0100

    Course updates

---
+++ section-2/hello.js
@@ -0,0 +1,15 @@
+/* function findMeaning() {
+	return 42;
+}
+
+console.log(findMeaning());
+Object.assign(); */
+
+
+const mission = process.argv[2];
+
+if (mission === 'learn') {
+	console.log('Time to write some Node code!');
+} else {
+	console.log(`Is ${mission} really more fun?`);
+}
